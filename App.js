86baf74fb07627e7633ba73c82f18f7154cb5d5f
Tuomas Jaakola <tuomas.jaakola@iki.fi>
2018-10-31 19:51:23 +0200

    Subscribe to topic

--- App.js
+++ App.js
@@ -1,62 +1,65 @@
 import React from 'react';
-import { StyleSheet, Platform, Image, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Image, Text, View, ScrollView } from 'react-native';
 
 import firebase from 'react-native-firebase';
 
 export default class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      topic: ''
+    };
   }
 
   async componentDidMount() {
-    // TODO: You: Do firebase things
-    // const { user } = await firebase.auth().signInAnonymously();
-    // console.warn('User -> ', user.toJSON());
-
-    // await firebase.analytics().logEvent('foo', { bar: '123'});
+    const enabled = await firebase.messaging().hasPermission();
+    if (enabled) {
+      // user has permissions
+      console.warn('You have permission to use messaging.');
+      const topic = 'test001';
+      await firebase.messaging().subscribeToTopic(topic);
+      console.warn('Subscribed to topic', topic);
+      // save topic into state
+      this.setState({
+        topic: topic
+      });
+    } else {
+      // user doesn't have permission
+      console.warn('You have no permission to use messaging');
+      // ask for permission
+      try {
+        await firebase.messaging().requestPermission();
+        // User has authorised
+        console.warn('You gave permission to use messaging. Restart app.');
+      } catch (error) {
+        // User has rejected permissions
+        console.warn('You rejected permission to use messaging');
+      }
+    }
   }
 
   render() {
+    // read topic from state
+    const { topic } = this.state;
+    // get a boolean flag to make conditional rendering
+    const registered = topic !== '';
     return (
       <ScrollView>
         <View style={styles.container}>
           <Image source={require('./assets/ReactNativeFirebase.png')} style={[styles.logo]}/>
           <Text style={styles.welcome}>
-            Welcome to {'\n'} React Native Firebase
+            Notification Demo
           </Text>
+
           <Text style={styles.instructions}>
-            To get started, edit App.js
+            To see the notifications, this app must{'\n'}be closed or put in the background
           </Text>
-          {Platform.OS === 'ios' ? (
+
+          {registered &&
             <Text style={styles.instructions}>
-              Press Cmd+R to reload,{'\n'}
-              Cmd+D or shake for dev menu
+              This device is registered to receive{'\n'}notifications under topic {topic}
             </Text>
-          ) : (
-            <Text style={styles.instructions}>
-              Double tap R on your keyboard to reload,{'\n'}
-              Cmd+M or shake for dev menu
-            </Text>
-          )}
-          <View style={styles.modules}>
-            <Text style={styles.modulesHeader}>The following Firebase modules are pre-installed:</Text>
-            {firebase.admob.nativeModuleExists && <Text style={styles.module}>admob()</Text>}
-            {firebase.analytics.nativeModuleExists && <Text style={styles.module}>analytics()</Text>}
-            {firebase.auth.nativeModuleExists && <Text style={styles.module}>auth()</Text>}
-            {firebase.config.nativeModuleExists && <Text style={styles.module}>config()</Text>}
-            {firebase.crashlytics.nativeModuleExists && <Text style={styles.module}>crashlytics()</Text>}
-            {firebase.database.nativeModuleExists && <Text style={styles.module}>database()</Text>}
-            {firebase.firestore.nativeModuleExists && <Text style={styles.module}>firestore()</Text>}
-            {firebase.functions.nativeModuleExists && <Text style={styles.module}>functions()</Text>}
-            {firebase.iid.nativeModuleExists && <Text style={styles.module}>iid()</Text>}
-            {firebase.invites.nativeModuleExists && <Text style={styles.module}>invites()</Text>}
-            {firebase.links.nativeModuleExists && <Text style={styles.module}>links()</Text>}
-            {firebase.messaging.nativeModuleExists && <Text style={styles.module}>messaging()</Text>}
-            {firebase.notifications.nativeModuleExists && <Text style={styles.module}>notifications()</Text>}
-            {firebase.perf.nativeModuleExists && <Text style={styles.module}>perf()</Text>}
-            {firebase.storage.nativeModuleExists && <Text style={styles.module}>storage()</Text>}
-          </View>
+          }
         </View>
       </ScrollView>
     );
@@ -87,16 +90,4 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
-  modules: {
-    margin: 20,
-  },
-  modulesHeader: {
-    fontSize: 16,
-    marginBottom: 8,
-  },
-  module: {
-    fontSize: 14,
-    marginTop: 4,
-    textAlign: 'center',
-  }
 });
